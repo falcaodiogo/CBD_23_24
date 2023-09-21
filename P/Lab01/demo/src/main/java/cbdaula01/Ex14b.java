37ff9e26b0be95a0061bc5e619948813c5a83988
falcaodiogo <falcao.diogo@ua.pt>
2023-09-21 11:10:38 +0100

    aula 21-set

--- P/Lab01/demo/src/main/java/cbdaula01/Ex14b.java
+++ P/Lab01/demo/src/main/java/cbdaula01/Ex14b.java
@@ -1,57 +1,51 @@
 package cbdaula01;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import redis.clients.jedis.Jedis;
 
 public class Ex14b {
 
+    public static String USERS = "users2"; // Key set for users' name 
+
     public static void main(String[] args) throws IOException {
 
-        File file = new File("P/Lab01/nomes-pt-2021.csv");
+        File file = new File("P/Lab01/names.txt");
         Scanner sc = new Scanner(System.in);
         Jedis jedis = new Jedis();
-        try (FileWriter myWriter = new FileWriter("CBD-14b-out.txt")) {
-            List <String> names = new ArrayList<>();
+        FileWriter myWriter = new FileWriter("CBD-14b-out.txt");
 
-            try (Scanner input = new Scanner(file)) {
+        try (Scanner input = new Scanner(file)) {
 
-                while (input.hasNextLine()) {
-                    String[] infoo = input.nextLine().split(";");
-                    String name_info = infoo[0]; // name
-                    Integer rank_info = Integer.parseInt(infoo[1]); // rank
-                    jedis.zadd("names", rank_info, name_info); // automaticamente ordenado
+            while (true) {
+                System.out.println("Search for (Press Enter to quit): ");
+                String name = sc.nextLine();
+                if (name.isEmpty()) {
+                    break; 
                 }
 
-                while (true) {
-                    System.out.println("Search for (Press Enter to quit): ");
-                    String name = sc.nextLine();
-                    if (name.isEmpty()) {
-                        break; 
-                    }
+            name = name.toLowerCase();              
 
-                    names = jedis.zrevrange("names", 0, -1);
+            myWriter.write("Search for (Press Enter to quit): " + name + "\n");
 
-                    myWriter.write("Search for (Press Enter to quit): " + name + "\n");
-
-                    for(String word: names) {
-                        if (word.startsWith(name)) {
-                            System.out.println(word);
-                            myWriter.write(word + "\n");
-                        }
-                    }
-
-                }
-
+            while (input.hasNextLine()) {
+                String[] infoo = input.nextLine().split(";");
+                String name_info = infoo[0]; // name
+                Integer rank_info = Integer.parseInt(infoo[1]); // rank
+                jedis.zadd(USERS, rank_info, name_info); // automaticamente ordenado
             }
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
+
+                System.out.println(jedis.zrangeByLex(USERS, "[" + name, "(" + name + "~"));
+            }
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        } 
+        
 
         sc.close();
         jedis.close();
